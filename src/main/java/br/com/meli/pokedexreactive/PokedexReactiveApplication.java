62ec51058bdcabd11bad0b096938897742c96c0b
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-02-27 20:56:55 -0300

    start of project

---
+++ src/main/java/br/com/meli/pokedexreactive/PokedexReactiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.pokedexreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexReactiveApplication.class, args);
+    }
+
+}
