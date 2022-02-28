5a196f78b79fdf1c2cc9518dac1d021447482a17
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-02-27 22:34:29 -0300

    feat: add PUT and GET to controller

--- src/main/java/br/com/meli/pokedexreactive/controller/PokemonController.java
+++ src/main/java/br/com/meli/pokedexreactive/controller/PokemonController.java
@@ -14,29 +14,44 @@ public class PokemonController {
 
     private PokemonRepository pokemonRepository;
 
-    public PokemonController(PokemonRepository pokemonRepository){
+    public PokemonController(PokemonRepository pokemonRepository) {
         this.pokemonRepository = pokemonRepository;
     }
 
     @GetMapping("/get/all")
-    public ResponseEntity<Flux<Pokemon>> getAllPokemons(){
+    public ResponseEntity<Flux<Pokemon>> getAllPokemons() {
         return ResponseEntity.ok().body(pokemonRepository.findAll());
     }
 
     @GetMapping("/get/{id}")
-    public Mono<ResponseEntity<Pokemon>> getPokemonById(@PathVariable String id){
+    public Mono<ResponseEntity<Pokemon>> getPokemonById(@PathVariable String id) {
         return pokemonRepository.findById(id)
                 .map(pokemon -> ResponseEntity.ok(pokemon))
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @PostMapping("/post")
-    public Mono<ResponseEntity<Pokemon>> postPokemon(@RequestBody Pokemon pokemon){
-        return pokemonRepository.save(pokemon).thenReturn(ResponseEntity.status(HttpStatus.CREATED).build());
+    public Mono<ResponseEntity<Pokemon>> postPokemon(@RequestBody Pokemon pokemon) {
+        return pokemonRepository.save(pokemon).thenReturn(new ResponseEntity(pokemon, HttpStatus.CREATED));
+    }
+
+    @PutMapping("/put/{id}")
+    public Mono<ResponseEntity<Pokemon>> putPokemon(@PathVariable(value = "id") String id, @RequestBody Pokemon pokemon) {
+        return pokemonRepository.update(id, pokemon)
+                .map(x -> ResponseEntity.ok(x))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Mono<ResponseEntity<Void>> deletePokemon(@PathVariable(value = "id") String id) {
+        return pokemonRepository.findById(id).flatMap(
+                existingPokemon -> pokemonRepository.delete(existingPokemon)
+                        .then(Mono.just(ResponseEntity.ok().<Void>build()))
+        ).defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/ping")
-    public String pong(){
+    public String pong() {
         return "pong";
     }
 }
--- src/main/java/br/com/meli/pokedexreactive/repository/PokemonRepository.java
+++ src/main/java/br/com/meli/pokedexreactive/repository/PokemonRepository.java
@@ -3,7 +3,19 @@ package br.com.meli.pokedexreactive.repository;
 import br.com.meli.pokedexreactive.model.Pokemon;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface PokemonRepository extends ReactiveMongoRepository<Pokemon, String> {
+
+    default Mono<Pokemon> update(String id, Pokemon pokemon){
+        Mono<Pokemon> result = findById(id);
+        return result.map(pkmn -> {
+            pkmn.setNome(pokemon.getNome());
+            pkmn.setCategoria(pokemon.getCategoria());
+            pkmn.setHabilidade(pokemon.getHabilidade());
+            pkmn.setPeso(pokemon.getPeso());
+            return pkmn;
+        });
+    }
 }
